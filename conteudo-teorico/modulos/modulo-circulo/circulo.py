fb4df9967a5d19c098766f545f0bdc0280f1116d
Anne Karoline Fortunato do Carmo <annekarolinefc@gmail.com>
2022-08-18 15:39:20 -0300

    Trabalhando com Módulos

---
+++ conteudo-teorico/modulos/modulo-circulo/circulo.py
@@ -0,0 +1,13 @@
+pi = 3.14159
+
+def area(raio):
+    return pi*(raio**2)
+
+def perimetro(raio):
+    return 2*pi*raio 
+
+def superficie(raio):
+    return 4.0*area(raio)
+
+def volume(raio):
+    return (4.0/3.0)*pi*(raio**3)
---
+++ conteudo-teorico/modulos/modulo-circulo/principal.py
@@ -0,0 +1,9 @@
+#USANDO O MODULO
+import circulo as cr 
+
+from circulo import perimetro
+
+raio = float(input('Digite o raio do circulo:'))
+print('Area do circulo: ', cr.area(raio))
+print('Perimetro do circulo: ', cr.perimetro(raio))
+print('Volume do circulo: ', cr.volume(raio))
